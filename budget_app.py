3671be23de23b19a82f19fca6c51cec314c3a14c
Aslam p a <93756533+LW1117@users.noreply.github.com>
2024-01-19 20:47:32 +0530

    Created _str__ function for printing object

--- budget_app.py
+++ budget_app.py
@@ -1,5 +1,18 @@
 class Category:
     balance = 0
+
+    def __str__(self):
+        no_of_stars = int((30 - len(self.category_name)) / 2)
+        title_line = f'{"*" * no_of_stars }{self.category_name}{"*" * no_of_stars }'
+        content = ''
+        space = 0
+        for x in self.ledger:
+            space = 30 - len(x["description"][:23]) - len(str(x["amount"])[:7])
+            content += (f'{x["description"][:23]}{space * " "}{str(x["amount"])[:7]}\n')
+
+        output = f'{title_line}\n{content}Total:{self.balance}'
+        return output
+
     def __init__(self, category_name):
         self.category_name = category_name
         self.ledger = []
@@ -8,15 +21,15 @@ class Category:
         self.amount = amount
         self.balance += amount
         self.description = description
-        self.ledger.append({"amount": -amount, "description": description})
+        self.ledger.append({"amount": amount, "description": description})
     
     def withdraw(self, amount, description = ''):
         self.amount = amount
         self.description = description
         if not self.check_funds(amount):
             return False
-        self.balance += -amount
-        self.ledger.append({"amount": amount, "description": description})
+        self.balance -= amount
+        self.ledger.append({"amount": -amount, "description": description})
         return True
     
     def get_balance(self):
@@ -34,16 +47,34 @@ class Category:
             return False
         return True
 
-food = Category("food")
-travel = Category("travel")
-food.deposit(2000, 'hello')
-food.withdraw(2000, 'hello')
-food.deposit(2000, 'hello')
-food.transfer(1000,travel)
+# food = Category("food")
+# travel = Category("travel")
+# food.deposit(2000, 'hello')
 # food.withdraw(2000, 'hello')
-print(food.ledger)
-print(travel.balance)
-print(travel.check_funds(1000))
+# food.deposit(2000, 'hello')
+# food.transfer(1000,travel)
+# # food.withdraw(2000, 'hello')
+# print(food.ledger)
+# print(travel.balance)
+# print(travel)
+
+food = Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+print(food.get_balance())
+clothing = Category("Clothing")
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+clothing.withdraw(100)
+auto = Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
+
+print(food)
+print(clothing)
+
+# print(create_spend_chart([food, clothing, auto]))
  
 # def create_spend_chart(categories):
 #     pass
