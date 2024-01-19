5eaff18471a7adc36e92d089af2a6b94e73260c2
Aslam p a <93756533+LW1117@users.noreply.github.com>
2024-01-19 23:23:33 +0530

    Created create_spend_chart function

--- budget_app.py
+++ budget_app.py
@@ -3,14 +3,14 @@ class Category:
 
     def __str__(self):
         no_of_stars = int((30 - len(self.category_name)) / 2)
-        title_line = f'{"*" * no_of_stars }{self.category_name}{"*" * no_of_stars }'
+        title_line = f'*{"*" * no_of_stars }{self.category_name}{"*" * no_of_stars }'
         content = ''
         space = 0
         for x in self.ledger:
             space = 30 - len(x["description"][:23]) - len(str(x["amount"])[:7])
             content += (f'{x["description"][:23]}{space * " "}{str(x["amount"])[:7]}\n')
 
-        output = f'{title_line}\n{content}Total:{self.balance}'
+        output = f'{title_line}\n{content}Total:{self.balance:.2f}'
         return output
 
     def __init__(self, category_name):
@@ -21,7 +21,7 @@ class Category:
         self.amount = amount
         self.balance += amount
         self.description = description
-        self.ledger.append({"amount": amount, "description": description})
+        self.ledger.append({"amount": float("{:.2f}".format(amount)), "description": description})
     
     def withdraw(self, amount, description = ''):
         self.amount = amount
@@ -48,37 +48,79 @@ class Category:
         return True
 
 def create_spend_chart(category_list):
-    pass
+    bargraph = 'Percentage spent by category\n'
+    graph = {
+        100:['100|'],
+        90:[' 90|'],
+        80:[' 80|'],
+        70:[' 70|'],
+        60:[' 60|'],
+        50:[' 50|'],
+        40:[' 40|'],
+        30:[' 30|'],
+        20:[' 20|'],
+        10:[' 10|'],
+        0:['  0|'],
+    }
+    Percentages = []
+    category_names = [x.category_name for x in category_list]
+    temp = 0
+    spend_sum = 0
+    for category in category_list:
+        for item in category.ledger:
+            if str(item['amount'])[0] == '-':
+                spend_sum += item['amount']
+        Percentages.append(spend_sum)
+        spend_sum = 0
+    total_spend = sum(Percentages)
+    # print(total_spend)
+    for item in range(len(Percentages)):
+        Percentages[item] = round(round(Percentages[item] * 100 / total_spend) /10 ) * 10
+    # print(Percentages)
 
-# food = Category("food")
-# travel = Category("travel")
-# food.deposit(2000, 'hello')
-# food.withdraw(2000, 'hello')
-# food.deposit(2000, 'hello')
-# food.transfer(1000,travel)
-# # food.withdraw(2000, 'hello')
-# print(food.ledger)
-# print(travel.balance)
-# print(travel)
+    for item in range(0,110,10):
+        # print(item)
+        for percentage_no in range(len(Percentages)):
+            if Percentages[percentage_no] >= 0:
+                graph[item].append('o ')
+            Percentages[percentage_no] -= 10
+    
+    
+
+
+
+    for x in graph:
+        for y in graph[x]:
+            bargraph += f'{y} '
+        bargraph += '\n'
+    bargraph += '    -'
+    for percentage_no in Percentages:
+        bargraph += '---'
+    bargraph += '\n'
+
+    max_length = max(len(name) for name in category_names)
+    for i in range(max_length):
+        bargraph += '     '
+        for name in category_names:
+            if i < len(name):
+                bargraph += f'{name[i]}  '
+            else:
+                bargraph += '   '  # Padding for shorter names
+        bargraph += '\n'
+    return bargraph
 
 food = Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
-
-print(food)
-print(clothing)
-
-category_list = [food, clothing, auto]
-print(create_spend_chart(category_list))
+entertainment = Category("Entertainment")
+business = Category("Business")
+food.deposit(900, "deposit")
+entertainment.deposit(900, "deposit")
+business.deposit(900, "deposit")
+food.withdraw(105.55)
+entertainment.withdraw(33.40)
+business.withdraw(10.99)
+actual = create_spend_chart([business, food, entertainment])
+print(actual)
+print(entertainment)
 
 # print(create_spend_chart([food, clothing, auto]))
  
