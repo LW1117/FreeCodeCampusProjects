cbca0c678046fb59183dcee22c9c2aaac28c7081
Aslam p a <93756533+LW1117@users.noreply.github.com>
2024-01-20 15:44:48 +0530

    Create subclass squre of class rectangle

--- polygon_area_calculator.py
+++ polygon_area_calculator.py
@@ -1,14 +1,14 @@
 class Rectangle:
     
     def __init__(self, width, height):
+        self.set_width(width)
+        self.set_height(height)
+
+    def set_width(self, width):
         self.width = width
+
+    def set_height(self, height):
         self.height = height
-
-    def set_width():
-        pass
-
-    def set_height():
-        pass
 
     def get_area(self):
         return self.width * self.height
@@ -17,26 +17,67 @@ class Rectangle:
         return 2 * self.width + 2 * self.height
     
     def get_diagonal(self):
-        return (self.width ** 2 + self.height ** 2) ** 0.5
+        if self.width != self.height:
+            return (self.width ** 2 + self.height ** 2) ** 0.5
+        else:
+            return self.width * (2 ** 0.5)
     
     def get_picture(self):
         if self.width <= 50 and self.height <= 50:
-            row_text = '*' * self.width
-            col_text = (f'*{" " * (self.width - 2)}*\n') * (self.height - 2)
-            return f'{row_text}\n{col_text}{row_text}'
+            row_text = '*' * self.width + '\n'
+            return f'{row_text * self.height}'
         else:
             return 'Too big for picture.'
 
     def get_amount_inside(self, other_shape):
-        return other_shape.get_area() / self.get_area()
+        return int(self.get_area() / other_shape.get_area())
     
     def __str__(self):
         return f'Rectangle(width={self.width}, height={self.height})'
 
 
-class Square:
-    pass
+class Square(Rectangle):
 
-shape1 = Rectangle(5,5)
-shape2 = Rectangle(10,10)
-print(shape2.get_picture())
+    def __init__(self, side):
+        super().__init__(side, side)
+        self.side = side
+
+    def set_side(self, side):
+        self.side = side
+        super().set_height(side)
+        super().set_width(side)
+    
+    def set_width(self, side):
+        self.set_side( side)
+
+    def set_height(self, side):
+        self.set_side( side)
+
+    def __str__(self):
+        return f'Square(side={self.side})'
+
+rect = Rectangle(10, 5)
+# print(rect.get_area())
+# rect.set_height(3)
+# print(rect.get_perimeter())
+# print(rect)
+# print(rect.get_picture())
+
+sq = Square(9)
+# print(sq.get_area())
+# sq.set_side(4)
+# print(sq.get_diagonal())
+# print(sq)
+# print(sq.get_picture())
+
+# rect.set_height(8)
+# rect.set_width(16)
+# print(rect.get_amount_inside(sq))
+
+
+sq.set_width(4)
+rect.set_width(7)
+rect.set_height(3)
+rect.get_picture()
+print(sq)
+print(rect)
